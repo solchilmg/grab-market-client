216b4a3845353818d6788ab9869baf87d4c07893
solchilmg <solchi@nate.com>
2021-05-29 21:01:41 +0900

    반응형 웹 구현

--- src/product/index.js
+++ src/product/index.js
@@ -2,31 +2,49 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./index.css";
+import { API_URL } from "../config/constants";
+import dayjs from "dayjs";
+import { Button, message } from "antd";
 
 function ProductPage() {
   const { id } = useParams();
   const [product, setproduct] = useState(null);
-  useEffect(function () {
+
+  const getProduct = () => {
     axios
-      .get(
-        `https://55194d5c-440b-4a6f-b5c0-2eed7fafe4ff.mock.pstmn.io/products/${id}`
-      )
+      .get(`${API_URL}/products/${id}`)
       .then(function (result) {
-        setproduct(result.data);
+        setproduct(result.data.product);
       })
       .catch(function (error) {
         console.error(error);
       });
+  };
+
+  useEffect(function () {
+    getProduct();
   }, []);
 
   if (product === null) {
     return <h1>상품 정보를 받고 있습니다...</h1>;
   }
 
+  const onClickPurchase = () => {
+    axios
+      .post(`${API_URL}/purchase/${id}`)
+      .then((result) => {
+        message.info("구매가 완료되었습니다.");
+        getProduct();
+      })
+      .catch((error) => {
+        message.error(`에러가 발생했습니다. ${error.message}`);
+      });
+  };
+
   return (
     <div>
       <div id="image-box">
-        <img src={"/" + product.imageurl} />
+        <img src={`${API_URL}/${product.imageUrl}`} />
       </div>
       <div id="profile-box">
         <img src="/images/icons/avatar.png" />
@@ -35,8 +53,20 @@ function ProductPage() {
       <div id="contents-box">
         <div id="name">{product.name}</div>
         <div id="price">{product.price}</div>
-        <div id="createdAt">2020년 12월 8일</div>
-        <div id="description">{product.description}</div>
+        <div id="createdAt">
+          {dayjs(product.createdAt).format("YYYY년 MM월 DD일")}
+        </div>
+        <Button
+          id="purchase-button"
+          size="large"
+          type="primary"
+          danger
+          onClick={onClickPurchase}
+          disabled={product.soldout === 1}
+        >
+          속사포 구매
+        </Button>
+        <pre id="description">{product.description}</pre>
       </div>
     </div>
   );
